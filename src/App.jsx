c3229ef316e790aa5818d45fa552b8277b158e4a
Fabiana <andreafabianamogollonlazaro@gmail.com>
2026-04-11 11:43:28 -0500

    fix: app file

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Intro from "./pages/Intro";
-import NotFound from "./pages/NotFound";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/intro" element={<Intro />} />
-        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
