1f9c804846e9668c81d0eeaa48cf55b9777a223e
Fabiana <andreafabianamogollonlazaro@gmail.com>
2026-04-11 12:17:46 -0500

    fix: home problems

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import("tailwindcss").Config} */
 export default {
-  prefix: "tw-",
   content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        title: ["Anton", "sans-serif"],
+        body: ["DM Sans", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
