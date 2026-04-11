9d51471c7a2d16768eac7e51b99ae39ac73b58a6
Fabiana <andreafabianamogollonlazaro@gmail.com>
2026-04-11 11:08:10 -0500

    docs: add new pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Intro from "./pages/Intro";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes></Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/intro" element={<Intro />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </BrowserRouter>
   );
 }
