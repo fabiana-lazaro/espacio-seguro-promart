a0332d506d8a6473c6feef5c12d451ca0eec1d30
Fabiana <andreafabianamogollonlazaro@gmail.com>
2026-04-12 17:21:01 -0500

    docs: add pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Intro from "./pages/Intro";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/intro" element={<Intro />} />
       </Routes>
     </BrowserRouter>
   );
