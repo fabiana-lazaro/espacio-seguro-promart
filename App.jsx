69659bef95bf920e01d0b6a05d95311773680bbb
mariniki1234 <121645127+mariniki1234@users.noreply.github.com>
2026-04-19 18:49:29 -0500

    maria

---
+++ App.jsx
@@ -0,0 +1,7 @@
+import Menu from "./components/menu"
+
+function App() {
+  return <Menu />
+}
+
+export default App
---
+++ game.jsx
@@ -0,0 +1,8 @@
+export default function Game() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>🎮 Juego iniciado</h1>
+      <p>Aquí va tu trivia</p>
+    </div>
+  )
+}
